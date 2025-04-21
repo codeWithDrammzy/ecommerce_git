cef856a8bd877ceb62f2f1c24a6e9261cf8bc659
Dawud Aliyu Ahmad <139690821+codeWithDrammzy@users.noreply.github.com>
2025-04-21 14:40:48 +0100

    add btn to cart in progress

--- ecommerce/store/models.py
+++ ecommerce/store/models.py
@@ -29,8 +29,23 @@ class Order(models.Model):
 	complete = models.BooleanField(default=False)
 	transaction_id = models.CharField(max_length=100, null=True)
 
-	def __str__(self):
-		return str(self.id)
+	@property
+	def get_cart_total(self):
+		orderitems = self.orderitem_set.all()
+		
+	
+		total = sum([item.get_total() for item in orderitems])
+		return total
+	
+
+	
+	@property
+	def get_cart_items(self):
+		orderitem = self.orderitem_set.all()
+		total =sum([item.quantity for item in orderitem])
+		return total
+	
+	
 
 class OrderItem(models.Model):
 	product = models.ForeignKey(Product, on_delete=models.SET_NULL, null=True)
@@ -38,6 +53,11 @@ class OrderItem(models.Model):
 	quantity = models.IntegerField(default=0, null=True, blank=True)
 	date_added = models.DateTimeField(auto_now_add=True)
 
+	
+	def get_total(self):
+		total = self.product.price * self.quantity
+		return total
+	
 
 class ShippingAddress(models.Model):
 	customer = models.ForeignKey(Customer, on_delete=models.SET_NULL, null=True)
--- ecommerce/store/views.py
+++ ecommerce/store/views.py
@@ -1,4 +1,5 @@
 from django.shortcuts import render,redirect
+from django.http import JsonResponse    
 from .models import  *
 
 
@@ -14,21 +15,42 @@ def store(request):
 
 
 def checkout(request):
-    return render(request, 'store/checkout.html')
+    if request.user.is_authenticated:
+        customer = request.user.customer
+        order, created = Order.objects.get_or_create(customer=customer, complete=False)
+        items = order.orderitem_set.all()
+    else:
+        items = []
+        order = {'get_cart_total': 0, 'get_cart_items': 0}
+
+    context = {
+        'items': items,
+        'order': order
+    }
+
+    return render(request, 'store/checkout.html', context)
+
 
 def cart(request):
     if request.user.is_authenticated:
         customer = request.user.customer
-        order , created = Order.objects.get_or_create(customer=customer, complete =False)
+        order, created = Order.objects.get_or_create(customer=customer, complete=False)
         items = order.orderitem_set.all()
         oreder_items = OrderItem.objects.count()
     else:
-        items=[]
+        items = []
+        order = {'get_cart_total': 0, 'get_cart_items': 0}
+        oreder_items = 0  # Initialize oreder_items to 0 for non-authenticated users
 
-    context ={'items':items,
-              'order_items':oreder_items
-              }
+    context = {
+        'items': items,
+        'order_items': oreder_items,
+        'order': order
+    }
     return render(request, 'store/cart.html', context)
 
-
+def updateItem(request):
+    
+    
+    return JsonResponse('Item added sucessfully', safe=False)
 # Create your views here.
